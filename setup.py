726b617f1742a99b61eb3e0d3f4224223bc1f40f
Trífero <atrifero@ladiaria.com.uy>
2023-01-23 16:06:03 -0300

    upgrade for django 2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "django-shorturls",
-    version = "2.0.0",
+    version = "2.0.1",
     url = 'https://github.com/bfirsh/django-shorturls',
     license = 'BSD',
     description = "A URL shortening app for Django.",
--- src/shorturls/templatetags/shorturl.py
+++ src/shorturls/templatetags/shorturl.py
@@ -2,19 +2,17 @@ from six.moves.urllib.parse import urljoin
 
 from django import template
 from django.conf import settings
-from django.core import urlresolvers
+from django.urls import reverse, NoReverseMatch
 from django.utils.safestring import mark_safe
 from shorturls import default_converter as converter, views
 
 
 class ShortURL(template.Node):
-
     @classmethod
     def parse(cls, parser, token):
         parts = token.split_contents()
         if len(parts) != 2:
-            raise template.TemplateSyntaxError(
-                "{0!s} takes exactly one argument".format(parts[0]))
+            raise template.TemplateSyntaxError("{0!s} takes exactly one argument".format(parts[0]))
         return cls(template.Variable(parts[1]))
 
     def __init__(self, obj):
@@ -37,30 +35,25 @@ class ShortURL(template.Node):
             return urljoin(settings.SHORT_BASE_URL, prefix + tinyid)
 
         try:
-            return urlresolvers.reverse(views.redirect, kwargs={
-                'prefix': prefix,
-                'tiny': tinyid
-            })
-        except urlresolvers.NoReverseMatch:
+            return reverse(views.redirect, kwargs={'prefix': prefix, 'tiny': tinyid})
+        except NoReverseMatch:
             return ''
 
     def get_prefix(self, model):
         if not hasattr(self.__class__, '_prefixmap'):
-            self.__class__._prefixmap = dict(
-                (m, p) for p, m in settings.SHORTEN_MODELS.items())
-        key = '{0!s}.{1!s}'.format(
-            model._meta.app_label, model.__class__.__name__.lower())
+            self.__class__._prefixmap = dict((m, p) for p, m in settings.SHORTEN_MODELS.items())
+        key = '{0!s}.{1!s}'.format(model._meta.app_label, model.__class__.__name__.lower())
         return self.__class__._prefixmap[key]
 
 
 class RevCanonical(ShortURL):
-
     def render(self, context):
         url = super(RevCanonical, self).render(context)
         if url:
             return mark_safe('<link rev="canonical" href="{0!s}">'.format(url))
         else:
             return ''
+
 
 register = template.Library()
 register.tag('shorturl', ShortURL.parse)
